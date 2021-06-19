21f9e6c186708c3464489c965f19ee3ef1aa4bb2
Viktor Martynov <2013alfabravo@gmail.com>
2021-06-19 15:21:43 +0400

    changed boxBackgroundColor for TextInputLayout to white

--- app/src/main/java/com/github/alfabravo2013/readyforexams/MainActivity.kt
+++ app/src/main/java/com/github/alfabravo2013/readyforexams/MainActivity.kt
@@ -1,18 +1,12 @@
 package com.github.alfabravo2013.readyforexams
 
-import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        window.apply {
-//            addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-//            statusBarColor = Color.parseColor("#FFFFFF")
-//        }
     }
 }
