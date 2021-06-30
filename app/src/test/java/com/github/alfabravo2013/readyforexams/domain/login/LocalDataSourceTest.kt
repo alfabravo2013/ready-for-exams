3736b122e4d4b5742d3607789cfadf1ce43272d5
Viktor Martynov <2013alfabravo@gmail.com>
2021-06-30 23:41:43 +0400

    add required dependencies

---
+++ app/src/test/java/com/github/alfabravo2013/readyforexams/domain/login/LocalDataSourceTest.kt
@@ -0,0 +1,21 @@
+package com.github.alfabravo2013.readyforexams.domain.login
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import org.koin.test.KoinTest
+
+internal class LocalDataSourceTest : KoinTest {
+
+    @Test
+    fun signUp() {
+    }
+
+    @Test
+    fun resetPassword() {
+    }
+
+    @Test
+    fun login() {
+    }
+}
