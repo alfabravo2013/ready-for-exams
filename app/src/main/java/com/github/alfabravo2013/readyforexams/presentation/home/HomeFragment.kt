e2ef697d1c8b40475ae6a925be238937bcf7cc85
Viktor Martynov <2013alfabravo@gmail.com>
2021-07-10 03:10:48 +0400

    fix TextInputLayout label background glitch

--- app/src/main/java/com/github/alfabravo2013/readyforexams/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/github/alfabravo2013/readyforexams/presentation/home/HomeFragment.kt
@@ -2,6 +2,7 @@ package com.github.alfabravo2013.readyforexams.presentation.home
 
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.github.alfabravo2013.readyforexams.R
 import com.github.alfabravo2013.readyforexams.databinding.FragmentHomeBinding
 import com.github.alfabravo2013.readyforexams.presentation.BaseFragment
@@ -14,5 +15,9 @@ class HomeFragment : BaseFragment(R.layout.fragment_home) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding = FragmentHomeBinding.bind(view)
         setToolbarTitle(requireContext().getString(R.string.home_title_text))
+
+        binding.homeAddChecklistButton.setOnClickListener {
+            findNavController().navigate(R.id.createFragment)
+        }
     }
 }
