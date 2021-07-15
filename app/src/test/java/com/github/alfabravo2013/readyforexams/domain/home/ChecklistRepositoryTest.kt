57fdde1f225070e033582a24c5cfc51cc398cb04
Viktor Martynov <2013alfabravo@gmail.com>
2021-07-15 15:59:14 +0400

    add ChecklistLocalDataSource tests

---
+++ app/src/test/java/com/github/alfabravo2013/readyforexams/domain/home/ChecklistRepositoryTest.kt
@@ -0,0 +1,5 @@
+package com.github.alfabravo2013.readyforexams.domain.home
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class ChecklistRepositoryTest
---
+++ app/src/test/java/com/github/alfabravo2013/readyforexams/presentation/home/HomeViewModelTest.kt
@@ -0,0 +1,5 @@
+package com.github.alfabravo2013.readyforexams.presentation.home
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class HomeViewModelTest
