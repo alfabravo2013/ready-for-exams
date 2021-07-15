0d57b1f1670a80a0f5d0e43f334361bd8f42b7e0
Viktor Martynov <2013alfabravo@gmail.com>
2021-07-16 00:20:07 +0400

    Revert "move home screen tests to a separate branch"
    This reverts commit 7d78a4070010ae016b1ed3d717a13a2be98a25e0.

---
+++ app/src/test/java/com/github/alfabravo2013/readyforexams/domain/home/ChecklistLocalDataSourceTest.kt
@@ -0,0 +1,164 @@
+package com.github.alfabravo2013.readyforexams.domain.home
+
+import com.github.alfabravo2013.readyforexams.domain.models.Checklist
+import com.github.alfabravo2013.readyforexams.domain.models.Task
+import com.github.alfabravo2013.readyforexams.util.Result
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+internal class ChecklistLocalDataSourceTest {
+    private val checklistLocalDataSource = ChecklistLocalDataSource()
+    private val checklist = Checklist("name", listOf())
+
+    @Nested
+    @DisplayName("When getChecklist")
+    inner class GetChecklistsTest {
+
+        @Test
+        @DisplayName("Given no checklists have been added yet, Then return empty list")
+        fun getChecklistsWhereNoHasBeenAddedYet() {
+            val checklists = checklistLocalDataSource.getChecklists()
+
+            assertEquals(0, checklists.size)
+        }
+
+        @Test
+        @DisplayName("Given a checklist has been added, Then return a list containing the single checklist")
+        fun getChecklistsWhereOneChecklistHasBeenAdded() {
+            checklistLocalDataSource.addChecklist(checklist)
+
+            val checklists = checklistLocalDataSource.getChecklists()
+
+            assertEquals(1, checklists.size)
+            assertEquals(checklist, checklists.first())
+        }
+
+        @Test
+        @DisplayName("Given a checklist has been added, Then the returned list contains only the added checklist")
+        fun getChecklistsShouldContainTheAddedChecklist() {
+            checklistLocalDataSource.addChecklist(checklist)
+
+            val checklists = checklistLocalDataSource.getChecklists()
+
+            assertEquals(checklist, checklists.first())
+        }
+    }
+
+    @Nested
+    @DisplayName("When addChecklist")
+    inner class AddChecklistTest {
+
+        @Test
+        @DisplayName("Given a unique checklist name Then return Result.Success")
+        fun addChecklistWithUniqueName() {
+            val result = checklistLocalDataSource.addChecklist(checklist)
+
+            assertTrue(result is Result.Success)
+        }
+
+        @Test
+        @DisplayName("Given a non-unique checklist name Then return Result.Failure")
+        fun addChecklistWithNonUniqueName() {
+            checklistLocalDataSource.addChecklist(checklist)
+
+            val result = checklistLocalDataSource.addChecklist(checklist)
+
+            assertTrue(result is Result.Failure)
+        }
+
+        @Test
+        @DisplayName("Given a unique checklist name Then the ChecklistLocalDataSource contains the added checklist")
+        fun addChecklistAddsTheChecklist() {
+            checklistLocalDataSource.addChecklist(checklist)
+
+            val result = checklistLocalDataSource.getChecklists().first()
+
+            assertEquals(checklist, result)
+        }
+    }
+
+    @Nested
+    @DisplayName("When updateChecklist")
+    inner class UpdateChecklistTest {
+
+        @Test
+        @DisplayName("Given an existing checklist name, Then return Result.Success")
+        fun updateExistingChecklist() {
+            checklistLocalDataSource.addChecklist(checklist)
+
+            val result = checklistLocalDataSource.updateChecklist(checklist)
+
+            assertTrue(result is Result.Success)
+        }
+
+        @Test
+        @DisplayName("Given an existing checklist name, Then the checklist is updated")
+        fun updateExistingChecklistWithNewData() {
+            val modifiedChecklist = Checklist("name", listOf(Task("task")))
+
+            checklistLocalDataSource.addChecklist(checklist)
+            checklistLocalDataSource.updateChecklist(modifiedChecklist)
+
+            val retrievedChecklist = checklistLocalDataSource.getChecklists().first()
+
+            assertEquals(modifiedChecklist, retrievedChecklist)
+        }
+
+        @Test
+        @DisplayName("Given a non-existing checklist, Then return Result.Failure")
+        fun updateNonExistingChecklist() {
+            val nonExistingChecklist = Checklist("another", listOf())
+
+            checklistLocalDataSource.addChecklist(checklist)
+
+            val result = checklistLocalDataSource.updateChecklist(nonExistingChecklist)
+
+            assertTrue(result is Result.Failure)
+        }
+
+        @Test
+        @DisplayName("Given a non-existing checklist, Then no checklist is updated")
+        fun updateNonExistingChecklistWithNewData() {
+            val nonExistingChecklist = Checklist("another", listOf(Task("task")))
+
+            checklistLocalDataSource.addChecklist(checklist)
+            checklistLocalDataSource.updateChecklist(nonExistingChecklist)
+
+            val retrievedChecklist = checklistLocalDataSource.getChecklists().first()
+
+            assertEquals(checklist, retrievedChecklist)
+        }
+    }
+
+    @Nested
+    @DisplayName("When deleteChecklistByName")
+    inner class DeleteChecklistByNameTest {
+
+        @Test
+        @DisplayName("Given an existing checklist name, Then the checklist is deleted")
+        fun deleteExistingChecklist() {
+            checklistLocalDataSource.addChecklist(checklist)
+
+            checklistLocalDataSource.deleteChecklistByName("name")
+
+            val checklists = checklistLocalDataSource.getChecklists()
+
+            assertEquals(0, checklists.size)
+        }
+
+        @Test
+        @DisplayName("Given a non-existing checklist name, Then no checklist is deleted")
+        fun deleteNonExistingChecklist() {
+            checklistLocalDataSource.addChecklist(checklist)
+
+            checklistLocalDataSource.deleteChecklistByName("another")
+
+            val checklists = checklistLocalDataSource.getChecklists()
+
+            assertEquals(1, checklists.size)
+        }
+    }
+}
---
+++ app/src/test/java/com/github/alfabravo2013/readyforexams/domain/home/ChecklistRepositoryTest.kt
@@ -0,0 +1,135 @@
+package com.github.alfabravo2013.readyforexams.domain.home
+
+import com.github.alfabravo2013.readyforexams.domain.models.Checklist
+import com.github.alfabravo2013.readyforexams.util.Result
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+internal class ChecklistRepositoryTest {
+    private val checklistLocalDataSource = mockk<ChecklistLocalDataSource>()
+    private val checklistRepository = ChecklistRepository(checklistLocalDataSource)
+
+    private val checklist = Checklist("name", listOf())
+
+    @Nested
+    @DisplayName("When getChecklists")
+    inner class GetChecklistsTest {
+
+        @Test
+        @DisplayName("When no checklists have been added yet, Then return empty list")
+        fun getChecklistsEmptyList() {
+            every { checklistLocalDataSource.getChecklists() } answers { emptyList() }
+
+            val checklists = checklistRepository.getChecklists()
+
+            assertEquals(0, checklists.size)
+
+            verify(exactly = 1) { checklistLocalDataSource.getChecklists() }
+        }
+
+        @Test
+        @DisplayName("When 1 checklist has been added, Then return a list size == 1")
+        fun getChecklistsNonEmptyList() {
+            every { checklistLocalDataSource.getChecklists() } answers { listOf(checklist) }
+
+            val checklists = checklistRepository.getChecklists()
+
+            assertEquals(1, checklists.size)
+
+            verify(exactly = 1) { checklistLocalDataSource.getChecklists() }
+        }
+
+        @Test
+        @DisplayName("When a checklist have been added, Then return a list contains this checklist")
+        fun getChecklistsContainsAddedChecklists() {
+            every { checklistLocalDataSource.getChecklists() } answers { listOf(checklist) }
+
+            val retrievedChecklists = checklistRepository.getChecklists()
+
+            assertEquals(listOf(checklist), retrievedChecklists)
+
+            verify(exactly = 1) { checklistLocalDataSource.getChecklists() }
+        }
+    }
+
+    @Nested
+    @DisplayName("When addChecklist")
+    inner class AddChecklistTest {
+
+        @Test
+        @DisplayName("Given a unique checklist name Then return Result.Success")
+        fun addChecklistWithUniqueName() {
+            every { checklistLocalDataSource.addChecklist(checklist) } returns Result.Success
+
+            val result = checklistRepository.addChecklist(checklist)
+
+            assertTrue(result is Result.Success)
+
+            verify(exactly = 1) { checklistLocalDataSource.addChecklist(checklist) }
+        }
+
+        @Test
+        @DisplayName("Given a non-unique checklist name Then return Result.Failure")
+        fun addChecklistWithNonUniqueName() {
+            every { checklistLocalDataSource.addChecklist(checklist) } returns Result.Failure()
+
+            val result = checklistRepository.addChecklist(checklist)
+
+            assertTrue(result is Result.Failure)
+
+            verify(exactly = 1) { checklistLocalDataSource.addChecklist(checklist) }
+        }
+    }
+
+    @Nested
+    @DisplayName("When updateChecklist")
+    inner class UpdateChecklistTest {
+
+        @Test
+        @DisplayName("Given an existing checklist name, Then return Result.Success")
+        fun updateExistingChecklist() {
+            every { checklistLocalDataSource.updateChecklist(checklist) } returns Result.Success
+
+            val result = checklistRepository.updateChecklist(checklist)
+
+            assertTrue(result is Result.Success)
+
+            verify(exactly = 1) { checklistLocalDataSource.updateChecklist(checklist) }
+        }
+
+        @Test
+        @DisplayName("Given a non-existing checklist name, Then return Result.Failure")
+        fun updateNonExistingChecklist() {
+            every { checklistLocalDataSource.updateChecklist(checklist) } returns Result.Failure()
+
+            val result = checklistRepository.updateChecklist(checklist)
+
+            assertTrue(result is Result.Failure)
+
+            verify(exactly = 1) { checklistLocalDataSource.updateChecklist(checklist) }
+        }
+    }
+
+    @Nested
+    @DisplayName("When deleteChecklistByName")
+    inner class DeleteChecklistByNameTest {
+
+        @Test
+        @DisplayName("given any checklist name, Then call ChecklistLocalDataSource.deleteChecklistByName with this name")
+        fun deleteChecklistByName() {
+            every { checklistLocalDataSource.deleteChecklistByName(any()) } just Runs
+
+            checklistRepository.deleteChecklistByName("name")
+
+            verify(exactly = 1) { checklistLocalDataSource.deleteChecklistByName("name") }
+        }
+    }
+}
