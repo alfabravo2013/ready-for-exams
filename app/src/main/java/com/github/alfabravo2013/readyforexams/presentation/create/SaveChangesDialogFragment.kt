7499a0c9187f2f6c5b47354c16f553914ac88812
Viktor Martynov <2013alfabravo@gmail.com>
2021-08-01 17:58:26 +0400

    add event listener to SaveChanges dialog

--- app/src/main/java/com/github/alfabravo2013/readyforexams/presentation/create/SaveChangesDialogFragment.kt
+++ app/src/main/java/com/github/alfabravo2013/readyforexams/presentation/create/SaveChangesDialogFragment.kt
@@ -4,24 +4,33 @@ import android.app.Dialog
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.github.alfabravo2013.readyforexams.R
+import com.github.alfabravo2013.readyforexams.presentation.create.SaveChangesDialogViewModel.OnEvent
 import org.koin.android.ext.android.inject
 
 class SaveChangesDialogFragment : DialogFragment() {
+
     private val viewModel: SaveChangesDialogViewModel by inject()
 
+    private val onEventObserver = Observer<OnEvent> { event ->
+        when (event) {
+            is OnEvent.NavigateToHomeScreen -> navigateToHomeScreen()
+        }
+    }
+
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        viewModel.onEvent.observe(this, onEventObserver)
+
         return AlertDialog.Builder(requireContext())
             .setTitle(getString(R.string.save_changes_dialog_title))
             .setMessage(getString(R.string.save_changes_dialog_message))
             .setPositiveButton(getString(R.string.save_changes_dialog_positive)) { _, _ ->
                 viewModel.onPositiveButtonClick()
-                navigateToHomeScreen()
             }
             .setNegativeButton(getString(R.string.save_changes_dialog_nagative)) { _, _ ->
                 viewModel.onNegativeButtonClick()
-                navigateToHomeScreen()
             }
             .setNeutralButton(getString(R.string.save_changes_dialog_neutral)) { _, _ ->
                 dismiss()
--- app/src/main/java/com/github/alfabravo2013/readyforexams/presentation/create/SaveChangesDialogViewModel.kt
+++ app/src/main/java/com/github/alfabravo2013/readyforexams/presentation/create/SaveChangesDialogViewModel.kt
@@ -1,5 +1,8 @@
 package com.github.alfabravo2013.readyforexams.presentation.create
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.github.alfabravo2013.readyforexams.domain.create.SaveChangesUseCase
@@ -9,11 +12,22 @@ class SaveChangesDialogViewModel(
     private val saveChangesUseCase: SaveChangesUseCase
 ) : ViewModel() {
 
+    private val _onEvent = MutableLiveData<OnEvent>()
+    val onEvent: LiveData<OnEvent> get() = _onEvent
+
     fun onPositiveButtonClick() = viewModelScope.launch {
         saveChangesUseCase.saveChanges()
+        _onEvent.value = OnEvent.NavigateToHomeScreen
+        Log.d("@#$", "onPositiveButtonClick: navigate to home screen dispatched")
     }
 
     fun onNegativeButtonClick() = viewModelScope.launch {
         saveChangesUseCase.discardChanges()
+        _onEvent.value = OnEvent.NavigateToHomeScreen
+        Log.d("@#$", "onNegativeButtonClick: navigate to home screen dispatched")
+    }
+
+    sealed class OnEvent {
+        object NavigateToHomeScreen : OnEvent()
     }
 }
