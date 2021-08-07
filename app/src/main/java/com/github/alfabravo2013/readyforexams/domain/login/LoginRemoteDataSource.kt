c1c5ef4701c36eea5ffaa5614eb0433aa94025c3
Viktor Martynov <2013alfabravo@gmail.com>
2021-08-08 01:26:18 +0400

    raname auth service variable

--- app/src/main/java/com/github/alfabravo2013/readyforexams/domain/login/LoginRemoteDataSource.kt
+++ app/src/main/java/com/github/alfabravo2013/readyforexams/domain/login/LoginRemoteDataSource.kt
@@ -3,16 +3,16 @@ package com.github.alfabravo2013.readyforexams.domain.login
 import com.github.alfabravo2013.readyforexams.networking.PasswordAuthService
 import com.github.alfabravo2013.readyforexams.util.Result
 
-class LoginRemoteDataSource(private val authProvider: PasswordAuthService) {
+class LoginRemoteDataSource(private val authService: PasswordAuthService) {
 
-    suspend fun login(email: String, password: String): Result = authProvider.login(email, password)
+    suspend fun login(email: String, password: String): Result = authService.login(email, password)
 
-    suspend fun resetPassword(email: String): Result = authProvider.resetPassword(email)
+    suspend fun resetPassword(email: String): Result = authService.resetPassword(email)
 
     suspend fun signup(email: String, password: String): Result =
-        authProvider.signup(email, password)
+        authService.signup(email, password)
 
-    suspend fun isLoggedIn(): Boolean = authProvider.isLoggedIn()
+    suspend fun isLoggedIn(): Boolean = authService.isLoggedIn()
 
-    suspend fun logout() = authProvider.logout()
+    suspend fun logout() = authService.logout()
 }
