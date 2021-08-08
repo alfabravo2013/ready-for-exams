a7bde79f09c647a3684bc72e216ce424a7cbe102
Viktor Martynov <2013alfabravo@gmail.com>
2021-08-08 11:58:12 +0400

    add coroutines-play-service for firebase

--- app/src/main/java/com/github/alfabravo2013/readyforexams/networking/FirebasePasswordAuthService.kt
+++ app/src/main/java/com/github/alfabravo2013/readyforexams/networking/FirebasePasswordAuthService.kt
@@ -1,45 +1,47 @@
 package com.github.alfabravo2013.readyforexams.networking
 
 import com.github.alfabravo2013.readyforexams.util.Result
-import com.google.android.gms.tasks.Task
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+import java.lang.Exception
 
 class FirebasePasswordAuthService : PasswordAuthService {
-    private val auth = Firebase.auth
-    private var result: Result? = null
+    private val auth = FirebaseAuth.getInstance()
 
-    override suspend fun login(email: String, password: String): Result {
-        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
-            result = getResult(task)
+    override suspend fun login(email: String, password: String): Result =
+        withContext(Dispatchers.IO) {
+            try {
+                auth.signInWithEmailAndPassword(email, password).await()
+                return@withContext Result.Success
+            } catch (e: Exception) {
+                return@withContext Result.Failure(e.message ?: "Unknown reason")
+            }
         }
-        return result ?: Result.Failure("No result available")
+
+    override suspend fun logout() = withContext(Dispatchers.IO) {
+        auth.signOut()
     }
 
-    override suspend fun logout() = auth.signOut()
-
-    override suspend fun signup(email: String, password: String): Result {
-        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
-            result = getResult(task)
+    override suspend fun signup(email: String, password: String): Result =
+        withContext(Dispatchers.IO) {
+            try {
+                auth.createUserWithEmailAndPassword(email, password).await()
+                return@withContext Result.Success
+            } catch (e: Exception) {
+                return@withContext Result.Failure(e.message ?: "Unknown reason")
+            }
         }
-        return result ?: Result.Failure("No result available")
-    }
 
-    override suspend fun resetPassword(email: String): Result {
-        auth.sendPasswordResetEmail(email).addOnCompleteListener { task ->
-            result = getResult(task)
+    override suspend fun resetPassword(email: String): Result = withContext(Dispatchers.IO) {
+        try {
+            auth.sendPasswordResetEmail(email).await()
+            return@withContext Result.Success
+        } catch (e: Exception) {
+            return@withContext Result.Failure(e.message ?: "Unknown reason")
         }
-        return result ?: Result.Failure("No result available")
     }
 
     override suspend fun isLoggedIn(): Boolean = auth.currentUser != null
-
-    private fun getResult(task: Task<*>): Result {
-        return if (task.isSuccessful) {
-            Result.Success
-        } else {
-            val reason = task.exception?.message ?: "Unknown reason"
-            Result.Failure(reason)
-        }
-    }
 }
